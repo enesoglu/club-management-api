17c827ef0ea130ff4fb3fb14ad9b0b86ae6adb89
enes yıldız <enesyildizonline@hotmail.com>
2025-07-21 15:41:43 +0300

    initial commit

---
+++ src/test/java/com/cms/clubmanagementapi/ClubManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cms.clubmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
