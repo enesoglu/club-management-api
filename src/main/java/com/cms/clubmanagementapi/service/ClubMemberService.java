bf38c9506824b920b6f15d19c95a7e0c60b5c896
enes yıldız <enesyildizonline@hotmail.com>
2025-08-01 17:56:54 +0300

    feat: implement import members via csv file

--- src/main/java/com/cms/clubmanagementapi/service/ClubMemberService.java
+++ src/main/java/com/cms/clubmanagementapi/service/ClubMemberService.java
@@ -1,32 +1,85 @@
 package com.cms.clubmanagementapi.service;
 
 import com.cms.clubmanagementapi.model.ClubMember;
-import com.cms.clubmanagementapi.repo.ClubMemberRepo;
+import com.cms.clubmanagementapi.model.MemberRole;
+import com.cms.clubmanagementapi.model.MemberStatus;
+import com.cms.clubmanagementapi.repository.ClubMemberRepository;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.transaction.annotation.Transactional;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ClubMemberService {
 
-    private final ClubMemberRepo clubMemberRepo;
+    private final ClubMemberRepository clubMemberRepository;
 
-    public ClubMemberService(ClubMemberRepo clubMemberRepo) {
-        this.clubMemberRepo = clubMemberRepo;
+    public ClubMemberService(ClubMemberRepository clubMemberRepository) {
+        this.clubMemberRepository = clubMemberRepository;
     }
 
     // get all members
     public List<ClubMember> findAllMembers(){
-        return clubMemberRepo.findAll();
+        return clubMemberRepository.findAll();
     }
 
     // create new member
     public ClubMember createMember(ClubMember member){
-        return clubMemberRepo.save(member);
+        return clubMemberRepository.save(member);
     }
 
     // delete a member
-    public void deleteMember(@PathVariable Long id) { clubMemberRepo.deleteById(id);}
+    public void deleteMember(@PathVariable Long id) { clubMemberRepository.deleteById(id);}
 
+    // create new members from csv file
+    @Transactional
+    public void saveMembersFromCsv(MultipartFile file) throws IOException {
+
+        final CSVFormat format = CSVFormat.DEFAULT.builder()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .setIgnoreHeaderCase(true)
+                .setTrim(true)
+                .build();
+
+        List<ClubMember> membersToSave = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+             CSVParser csvParser = CSVParser.parse(reader, format)) {
+
+            for (CSVRecord csvRecord : csvParser) {
+                ClubMember newMember = new ClubMember();
+
+                newMember.setFirstName(csvRecord.get("firstName"));
+                newMember.setLastName(csvRecord.get("lastName"));
+                newMember.setEmail(csvRecord.get("email"));
+                newMember.setPhoneNumber(csvRecord.get("phoneNumber"));
+                newMember.setMemberNo(Integer.parseInt(csvRecord.get("memberNo")));
+                newMember.setEmail(csvRecord.get("email"));
+                newMember.setYearOfStudy(csvRecord.get("yearOfStudy"));
+                newMember.setFaculty(csvRecord.get("faculty"));
+                newMember.setDepartment(csvRecord.get("department"));
+                newMember.setRole(MemberRole.MEMBER);
+                newMember.setMembershipStatus(MemberStatus.ACTIVE);
+
+                membersToSave.add(newMember);
+            }
+        }
+
+        if (!membersToSave.isEmpty()) {
+            clubMemberRepository.saveAll(membersToSave);
+        }
+    }
 }
