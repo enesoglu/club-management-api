559163b31cdda69c95bd439c2ca03f1061666a7d
enes yıldız <enesyildizonline@hotmail.com>
2025-08-22 10:43:46 +0300

    fix(dto): termController not returning DTOs

--- src/main/java/com/cms/clubmanagementapi/service/TermService.java
+++ src/main/java/com/cms/clubmanagementapi/service/TermService.java
@@ -1,30 +1,39 @@
 package com.cms.clubmanagementapi.service;
 
+import com.cms.clubmanagementapi.dto.request.CreateTermRequest;
+import com.cms.clubmanagementapi.dto.response.TermDTO;
+import com.cms.clubmanagementapi.mapper.TermMapper;
 import com.cms.clubmanagementapi.model.role.Term;
 import com.cms.clubmanagementapi.repository.TermRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class TermService {
 
     private final TermRepository termRepository;
+    private final TermMapper termMapper;
 
-    public TermService(TermRepository termRepository) {
+    public TermService(TermRepository termRepository,  TermMapper termMapper) {
         this.termRepository = termRepository;
+        this.termMapper = termMapper;
     }
 
-    public Optional<Term> findByIsActiveTrue() {
-        return termRepository.findByIsActiveTrue();
+    public TermDTO findActiveTerm() {
+        Term term = termRepository.findByIsActiveTrue()
+                .orElseThrow(()-> new EntityNotFoundException("Term not found"));
+        return termMapper.toDTO(term);
+    }
+
     }
 
     public void deleteById(@PathVariable Long id) { termRepository.deleteById(id); }
 
     @Transactional
-    public Term setActiveTerm(Long termId) {
+    public TermDTO setActiveTerm(Long termId) {
         // is there an active (isActive = true) term?
         termRepository.findByIsActiveTrue().ifPresent(oldActiveTerm -> {
             // if so, make it false.
@@ -38,6 +47,7 @@ public class TermService {
 
         // make the new term active (true).
         newActiveTerm.setActive(true);
-        return termRepository.save(newActiveTerm);
+        termRepository.save(newActiveTerm);
+        return termMapper.toDTO(newActiveTerm);
     }
 }
