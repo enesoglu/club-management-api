e808f8f15c1a7e5bca32f3af9605c6ae756fe2fa
Enes Yıldız <yildiz_enes@hotmail.com>
2025-09-01 16:26:23 +0300

    Add field to ClubMemberDTO
    Introduces a new 'nationalId' property.

--- src/main/java/com/cms/clubmanagementapi/dto/response/ClubMemberDTO.java
+++ src/main/java/com/cms/clubmanagementapi/dto/response/ClubMemberDTO.java
@@ -14,6 +14,7 @@ public class ClubMemberDTO {
     private String email;
     private String phone;
     private String yearOfStudy;
+    private String nationalId;
     private String faculty;
     private String department;
     private LocalDate registrationDate;
